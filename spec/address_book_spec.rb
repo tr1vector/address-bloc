385ecee30849bea69f4f929067dba67c30046c8a
tr1vector <tr1vector@outlook.com>
2017-11-29 21:54:12 -0700

    Checkpoint 7 Address Bloc Searching Complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -132,6 +132,64 @@ RSpec.describe AddressBook do
         check_entry(entry_three, "Nett", "555-555-2039", "nett@blocmail.com")
       end
     end
+
+    describe "#binary_search" do
+      let(:book) { AddressBook.new}
+
+      def check_entry(entry, expected_name, expected_number, expected_email)
+        expect(entry.name).to eq expected_name
+        expect(entry.phone_number).to eq expected_number
+        expect(entry.email).to eq expected_email
+      end
+
+      it "searches AddressBook for a non-existent entry" do
+        book.import_from_csv("entries.csv")
+        entry = book.binary_search("Dan")
+        expect(entry).to be_nil
+      end
+
+      it "searches AddressBook for Bill" do
+        book.import_from_csv("entries.csv")
+        entry = book.binary_search("Bill")
+        expect(entry).to be_a Entry
+        check_entry(entry, "Bill", "555-555-4854", "bill@blocmail.com")
+      end
+
+      it "searches AddressBook for Bob" do
+        book.import_from_csv("entries.csv")
+        entry = book.binary_search("Bob")
+        expect(entry).to be_a Entry
+        check_entry(entry, "Bob", "555-555-5415", "bob@blocmail.com")
+      end
+ 
+      it "searches AddressBook for Joe" do
+        book.import_from_csv("entries.csv")
+        entry = book.binary_search("Joe")
+        expect(entry).to be_a Entry
+        check_entry(entry, "Joe", "555-555-3660", "joe@blocmail.com")
+      end
+ 
+      it "searches AddressBook for Sally" do
+        book.import_from_csv("entries.csv")
+       entry = book.binary_search("Sally")
+        expect(entry).to be_a Entry
+        check_entry(entry, "Sally", "555-555-4646", "sally@blocmail.com")
+      end
+ 
+      it "searches AddressBook for Sussie" do
+        book.import_from_csv("entries.csv")
+        entry = book.binary_search("Sussie")
+        expect(entry).to be_a Entry
+        check_entry(entry, "Sussie", "555-555-2036", "sussie@blocmail.com")
+      end
+
+      it "searches AddressBook for Billy" do
+        book.import_from_csv("entries.csv")
+        entry = book.binary_search("Billy")
+        expect(entry).to be_nil
+      end
+    end
+
    	describe "#remove_entry" do
       let(:book) { AddressBook.new}
    		it "removes the entry from the address book" do
